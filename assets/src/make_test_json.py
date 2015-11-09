581e0877fb7596ec845587b3a652d85c68bfd32f
Alex Reynolds <alexpreynolds@gmail.com>
2015-11-09 00:29:05 -0800

    update json test generation to reflect current schema

--- assets/src/make_test_json.py
+++ assets/src/make_test_json.py
@@ -4,71 +4,88 @@ import json, random
 
 sample = {
     'metadata' : {
-        'title' : 'Random data points',
-        'subtitle' : 'Subtitle of random dataset',
-        'axis' : {
-            'x' : 'PC1',
-            'y' : 'PC2',
-            'z' : 'PC3'
+        'title' : 'Random points',
+        'subtitle' : 'Testing of randomly sampled points',
+        'axis': {
+            'x': {
+                'name': 'PC1',
+                'color': '#000000',
+                'thickness': 1,
+                'tick_color': '#000000',
+                'tick_thickness': 1,
+                'tick_length': 0.1
             },
-        'selected_class' : 'Lineage',
-        'show_axes' : False,
+            'y': {
+                'name': 'PC2',
+                'color': '#000000',
+                'thickness': 1,
+                'tick_color': '#000000',
+                'tick_thickness': 1,
+                'tick_length': 0.1
+            },
+            'z': {
+                'name': 'PC3',
+                'color': '#000000',
+                'thickness': 1,
+                'tick_color': '#000000',
+                'tick_thickness': 1,
+                'tick_length': 0.1
+            }
+        },
+        'selected_class': 'Lineage',
+        'show_axes': True,
+        'invert_y_axis': False,
+        'show_legend': True,
+        'show_title': True,
+        'particle_size': 0.16,
+        'rotation_speed': 0.006,
         'classes' : {
             'Lineage' : [
-                        {
-                            "name": "Paraxial mesoderm deratives",
-                            "rgb": [
-                                            238,
-                                            23,
-                                            23
-                                          ]
-                                    },
-                        {
-                            "name": "Lymphoid",
-                            "rgb": [
-                                            222,
-                                            99,
-                                            20
-                                          ]
-                                    },
-                        {
-                            "name": "Primitive",
-                            "rgb": [
-                                            128,
-                                            128,
-                                            255
-                                          ]
-                                    },
-                        {
-                            "name": "Ectoderm",
-                            "rgb": [
-                                            0,
-                                            0,
-                                            255
-                                          ]
-                                    }
-                      ]
-            },
+                {
+                    'name': 'Paraxial mesoderm deratives',
+                    'rgb': [
+                        238,
+                        23,
+                        23
+                ]
+                },
+                {
+                    'name': 'Lymphoid',
+                    'rgb': [
+                        222,
+                        99,
+                        20
+                ]
+                },
+                {
+                    'name': 'Primitive',
+                    'rgb': [
+                        128,
+                        128,
+                        255
+                ]
+                },
+                {
+                    'name': 'Ectoderm',
+                    'rgb': [
+                        0,
+                        0,
+                        255
+                    ]
+                }
+            ]
+        },
         'range' : {
             'x' : [ -1000.0, 1000.0 ],
             'y' : [ -1000.0, 1000.0 ],
             'z' : [ -1000.0, 1000.0 ]
-            }
-        },
-    'data' : [
-        {
-            "type" : {
-                "Lineage": 0
-            },
-            "id": "Point 0",
-            "x": -276.412663905422,
-            "y": -83.1758800539113,
-            "z": -25.0089776269546
         }
+    },
+    'data' : [
     ]
 }
 
-samples = 300
+samples = 100
 
 for i in xrange(samples):
     x_rand = random.uniform(sample['metadata']['range']['x'][0], sample['metadata']['range']['x'][1])
